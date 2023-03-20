834d3818c9950932d6e216bc44bd2d6af12423b8
Andrew Clarkson <andrew.clarkson@universe.com>
2023-03-20 19:53:28 -0400

    get single item from db

---
+++ src/pages/api/items/[itemId].ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient()
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req
+
+  switch (method) {
+    case 'GET':
+      const { itemId } = req.query
+      if (!itemId) {
+        res.status(400).json({message: 'missing itemId'})
+        break;
+      } else if (Array.isArray(itemId)) {
+        res.status(400).json({message: 'do not provide more than one itemId'})
+        break;
+      } else {
+        const item = await prisma.item.findFirst({ where: { id: itemId } })
+        res.status(200).json(item)
+        break
+      }
+    case 'POST':
+
+
+
+    default:
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
