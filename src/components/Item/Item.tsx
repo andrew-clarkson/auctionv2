dcdd762520470e54614a0da9bd4c7f73b62baf4a
Andrew Clarkson <andrew.clarkson@universe.com>
2023-03-23 17:21:10 -0400

    Start item card styling

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -29,19 +29,21 @@ export default function Item(props: Props) {
   return (
     <div className={styles.itemContainer}>
       <Image
+        className={styles.image}
         src={props.item.photoUrl}
         width={300}
         height={200}
         alt='auction pic'
       />
-      <div>
+      <div className={styles.detailsSection}>
         <p className={styles.title}>{props.item.title}</p>
-        <p className={styles.title}>{props.item.description}</p>
-        {/* <p className={styles.auctioneer}>{props.item.auctioneer}</p> */}
-        <p>${props.item.price}</p>
-        <button>Bid</button>
-        <Link href={`/editItem/${props.item.id}`} ><button>Edit</button></Link>
-        <button onClick={deleteItem}>Delete</button>
+        <p className={styles.description}>{props.item.description}</p>
+        <p>${props.item.price} 14 bids</p>
+        <div className={styles.buttonsSection}>
+          <button>Bid</button>
+          <Link href={`/editItem/${props.item.id}`} ><button>Edit</button></Link>
+          <button onClick={deleteItem}>Delete</button>
+        </div>
       </div>
     </div>
   );
