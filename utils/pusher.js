22ab9e77b90920bebd1a2284ee2f4d0520118648
Andrew Clarkson <andrew.clarkson@universe.com>
2023-04-06 20:33:16 -0400

    notes, import structure

--- utils/pusher.js
+++ utils/pusher.js
@@ -1,3 +1,5 @@
-Pusher.logToConsole = true;
 import Pusher from "pusher-js";
+
+Pusher.logToConsole = true;
+
 export const pusherConnection = new Pusher('48a91d99d52f3a702af0', { cluster: 'us2' });
