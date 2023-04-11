e3949a7e5bfda3e7461449e51c3d4ac841d9ab32
Andrew Clarkson <andrew.clarkson@universe.com>
2023-04-10 20:03:35 -0400

    update nextauth config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from 'utils/prisma'
+
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
